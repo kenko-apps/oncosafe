c3e1ccfd7ea0844420707329f6dd1894bede8dcd
ALLIO Cédric <allio@havre-port.lan>
2017-06-29 09:00:28 +0200

    Amélioration de la page 3 du formulaire

--- src/pages/formulaire-trois/formulaire-trois.ts
+++ src/pages/formulaire-trois/formulaire-trois.ts
@@ -14,14 +14,29 @@ export class FormulairePageTrois {
 
   slideTroisForm: FormGroup;
   submitAttempt: boolean = false;
+  labelFrequenceTabac: boolean = false;
+  questionsTabac: boolean = false;
   constructor(public navCtrl: NavController, public menu: MenuController, translate: TranslateService, public formBuilder: FormBuilder) {
     this.slideTroisForm = formBuilder.group({
         tabac_check:  ['', Validators.required],
+        frequence_tabac:  ['', Validators.required],
         date_tabac: ['', Validators.required],
         activite_check: ['', Validators.required]
     }); 
   }
 
+  fumeOui() {
+    this.questionsTabac = true;
+  }
+
+  fumeNon() {
+    this.questionsTabac = false;
+  }
+
+  changeLabel() {
+    this.labelFrequenceTabac = true;
+  }
+  
   nextPage() {
     this.submitAttempt = true;
     if(this.slideTroisForm.valid){
